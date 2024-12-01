dea78857325b64e0c506e414a89c4fa6b64811aa
Gaurav Verma <gverma0549@gmail.com>
2024-12-01 13:47:17 +0530

    Workng Fine without Loadbalancer

--- src/main/java/com/exchange/ExchangeApplication.java
+++ src/main/java/com/exchange/ExchangeApplication.java
@@ -3,10 +3,11 @@ package com.exchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 public class ExchangeApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExchangeApplication.class, args);
